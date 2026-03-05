1d854d21c2fb60038719453fecf58a3f5b7e7aa9
Truong Ngoc Minh <truongocminh204@gmail.com>
2026-03-05 12:03:50 +0700

    Initialize Spring Boot microservice

---
+++ src/main/java/com/example/xdwebcd1bemicroserviceproductservice/XdWebCd1BeMicroServiceProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.xdwebcd1bemicroserviceproductservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XdWebCd1BeMicroServiceProductServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XdWebCd1BeMicroServiceProductServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/xdwebcd1bemicroserviceproductservice/XdWebCd1BeMicroServiceProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.xdwebcd1bemicroserviceproductservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XdWebCd1BeMicroServiceProductServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
